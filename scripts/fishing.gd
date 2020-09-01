6cf961003fc56e06106df7aaa069af8e6c7c6e6e
cattythegamer-tcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2020-09-01 15:02:06 +1200

    Adjusted a fish position
    Was way to hard to get

--- scripts/fishing.gd
+++ scripts/fishing.gd
@@ -5,9 +5,15 @@ func _ready():
 	GV.scene = "fishing"
 	GV.fish_remaining = 10
 	GV.scene_end = false
+	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if GV.fish_remaining == 0:
 		GV.scene_end = true
+	$timerDisplay.text = str(round($Timer.time_left)) + "s"
+
+
+func _on_Timer_timeout():
+	GV.scene_end = true
