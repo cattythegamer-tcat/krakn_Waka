86d52630b4295657902dda50d5fb7bc19e49a758
cattythegamer-tcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2020-09-08 14:04:42 +1200

    Minor tweaks to Tangaroa

--- scripts/tangaroaBoss.gd
+++ scripts/tangaroaBoss.gd
@@ -1,20 +1,17 @@
 extends Spatial
 
-var firing_amt = 1
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var firing_amt = 2
+var rung = 0
 
 func hit():
-	if firing_amt != 0:
-		$collision.play("collide")
-	else:
-		firing_amt = 1
+	rung += 1
+	
+	if rung == firing_amt:
+		if firing_amt != 0:
+			$collision.play("collide")
+		firing_amt = 2
+		rung = 0
 
 func _on_orb_hit():
+	print("PING")
 	firing_amt -= 1
