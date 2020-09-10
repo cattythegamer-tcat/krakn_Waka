744e582444b604d0a59eeedc0ba8b569554f8edd
cattythegamer-tcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2020-09-10 15:08:25 +1200

    Added multifiring capabilities to Tangaroa

--- scripts/tangaroaBoss.gd
+++ scripts/tangaroaBoss.gd
@@ -1,16 +1,18 @@
 extends Spatial
 
-var firing_amt = 3
+const FIRING = 3
+
+var firing_amt = FIRING
 var rung = 0
 
 func hit():
 	rung += 1
 	
-	if rung == firing_amt:
+	if rung == FIRING:
 		if firing_amt != 0:
 			$collision.play("collide")
 			GV.food -= firing_amt * 2
-		firing_amt = 3
+		firing_amt = FIRING
 		rung = 0
 
 func _on_orb_hit():
