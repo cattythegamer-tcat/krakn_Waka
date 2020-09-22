449082629f827b2d4c7b1d8e6f8eba0da87771c3
Jayden <everesja@gmail.com>
2020-09-22 17:24:47 +1200

    Settings Tweaks

--- scripts/introductionCutscene.gd
+++ scripts/introductionCutscene.gd
@@ -2,11 +2,6 @@ extends Spatial
 
 var pos_num = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("continue") and !$move_camera.is_playing():
--- scripts/tangaroaBoss.gd
+++ scripts/tangaroaBoss.gd
@@ -18,30 +18,30 @@ func hit():
 		else:
 			$damage.play("tangaroaDamaged")
 			FIRING += 2
-			if FIRING == 3:
-				$minion_emerge.play("first_level")
-				$bossHealth.text = "Phase 2/4"
-			elif FIRING == 5:
-				$minion_emerge.play("second_level")
-				$bossHealth.text = "Phase 3/4"
-			elif FIRING == 7:
-				$minion_emerge.play("third_level")
-				$bossHealth.text = "Phase 4/4"
-			else:
-				$bossHealth.text = "DEFEATED"
-				$center_beam.queue_free()
-				$left_beam.queue_free()
-				$right_beam.queue_free()
-				$far_left_beam.queue_free()
-				$far_right_beam.queue_free()
-				$super_far_left_beam.queue_free()
-				$super_far_right_beam.queue_free()
-				$damage.play("dying")
+			match FIRING:
+				3:
+					$minion_emerge.play("first_level")
+					$bossHealth.text = "Phase 2/4"
+				5:
+					$minion_emerge.play("second_level")
+					$bossHealth.text = "Phase 3/4"
+				7:
+					$minion_emerge.play("third_level")
+					$bossHealth.text = "Phase 4/4"
+				_:
+					$bossHealth.text = "DEFEATED"
+					$center_beam.queue_free()
+					$left_beam.queue_free()
+					$right_beam.queue_free()
+					$far_left_beam.queue_free()
+					$far_right_beam.queue_free()
+					$super_far_left_beam.queue_free()
+					$super_far_right_beam.queue_free()
+					$damage.play("dying")
 		firing_amt = FIRING
 		rung = 0
 
 func _on_orb_hit():
-	print("PING")
 	firing_amt -= 1
 
 func end():
